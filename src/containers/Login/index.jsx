e70de54c56497deb3c9387b72817e93d3909b370
Airton-Sales <airtonsales0@gmail.com>
2024-05-17 17:09:17 -0300

    CONCTANDO FRONT END E BACK END

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -5,6 +5,7 @@ import * as yup from 'yup'
 
 import LoginImg from '../../assets/login-image.svg'
 import Logo from '../../assets/logo.svg'
+import api from '../../services/api'
 import {
   Container,
   LoginImage,
@@ -36,8 +37,12 @@ function Login() {
     resolver: yupResolver(schema)
   })
 
-  const onSubmit = data => {
-    console.log(data)
+  const onSubmit = async clientData => {
+    const res = await api.post('sessions', {
+      email: clientData.email,
+      password: clientData.password
+    })
+    console.log(res)
   }
   return (
     <Container>
