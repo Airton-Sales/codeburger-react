7a34f809713b53f000eee08dd5e64b23631ef2bf
Airton-Sales <airtonsales0@gmail.com>
2024-05-16 23:42:34 -0300

    ESTILIZANDO A TELA DE LOGIN

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 
+import LoginImg from '../../assets/login-image.svg'
+import Logo from '../../assets/logo.svg'
+import {
+  Container,
+  LoginImage,
+  ContainerItens,
+  Label,
+  Input,
+  Button,
+  SignInLik
+} from './styles'
+
 function Login() {
-  return <div>Login</div>
+  return (
+    <Container>
+      <LoginImage src={LoginImg} alt="login-image" />
+      <ContainerItens>
+        <img src={Logo} alt="Logo" />
+        <h1>Login</h1>
+
+        <Label>Email</Label>
+        <Input />
+
+        <Label>Senha</Label>
+        <Input />
+
+        <Button>Sign in</Button>
+
+        <SignInLik>
+          Não possui conta? ,<a>Sign Up</a>
+        </SignInLik>
+      </ContainerItens>
+    </Container>
+  )
 }
 
 export default Login
---
+++ src/containers/Login/styles.jsx
@@ -0,0 +1,87 @@
+import styled from 'styled-components'
+
+import Background from '../../assets/background.svg'
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url('${Background}');
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LoginImage = styled.img`
+  height: 70%;
+`
+
+export const ContainerItens = styled.div`
+  background-color: #373737;
+  border-radius: 0 10px 10px 0;
+  height: 70%;
+  padding: 25px 75px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h1 {
+    color: #fff;
+    font-family: Roboto;
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 28.13px;
+    text-align: center;
+    margin-top: 100px;
+  }
+`
+
+export const Label = styled.p`
+  color: #fff;
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 14.06px;
+  text-align: left;
+  margin-top: 28px;
+  margin-bottom: 5px;
+`
+
+export const Input = styled.input`
+  width: 391.42px;
+  height: 38.32px;
+  padding-left: 5px;
+  gap: 0px;
+  border-radius: 10px;
+  border: none;
+`
+
+export const Button = styled.button`
+  background: #9758a6;
+  border-radius: 20px;
+  border: none;
+  cursor: pointer;
+  width: 182.81px;
+  height: 36.13px;
+  color: #fff;
+  margin-top: 75px;
+  margin-bottom: 25px;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.5;
+  }
+`
+
+export const SignInLik = styled.p`
+  color: #fff;
+  font-size: 14px;
+  font-weight: 300;
+
+  a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
